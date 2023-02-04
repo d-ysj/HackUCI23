e8fede7cc2d64af0dc94f8ddbef0152001fe9763
Cayson Yim <yimck@uci.edu>
2023-02-04 12:46:31 -0800

    test4

--- public/script.js
+++ public/script.js
@@ -7,26 +7,10 @@ function callAPI(searchInput) {
 
 function searchRecipes() {
     let searchInput = document.getElementById('search').value;
-    /*searchInput.split(', ')
-    for (i=0; searchInput.length; i++) {
-        pass
-    }
-    //callAPI(searchInput);*/
-    
-    //fetch('https://api.edamam.com/api/recipes/v2?app_id=3780601a&app_key=410cafebce46de027659e5d8f0b7f695&type=public&q=' + searchInput, options)
-    /*fetch('/api/(.*)')
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-
-    /*fetch('')
-    .then(response => {return response.json();})
-    .then(response => {return;})*/
-
-    const options = {method: 'GET'};
+    var searchList = searchInput.split(', ')
 
     fetch('https://hack-uci-23.vercel.app/api/product', options)
-        .ten(response => response.json())
+        .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err))
 }
