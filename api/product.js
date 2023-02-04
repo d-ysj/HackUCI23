0b51b6ee91dcc5d332879b45959779b256ba3d72
WanderingSnail <seongjin123.SY@gmail.com>
2023-02-04 13:07:50 -0800

    david json

--- api/product.js
+++ api/product.js
@@ -5,23 +5,23 @@ let ingrList = 'chicken,tomato,pepper,cheese';
 const app = express();
 
 const options = {
-  method: 'GET',
-  url: 'https://api.spoonacular.com/recipes/findByIngredients',
-  qs: {
-    apiKey: '0ce380dcbd274d5b8203bbca9a7cb1b8',
-    ingredients: ingrList,
-    ignorePantry: 'true',
-    ranking: '2'
-  }
+    method: 'GET',
+    url: 'https://api.spoonacular.com/recipes/findByIngredients',
+    qs: {
+        apiKey: '0ce380dcbd274d5b8203bbca9a7cb1b8',
+        ingredients: ingrList,
+        ignorePantry: 'true',
+        ranking: '2'
+    }
 };
 //routes 
-app.get('/',(req,res) => {
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+app.get('/', (req, res) => {
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
 
-  console.log(body);
+        console.log(body);
 
-  res.json({response});
-});
+        res.json(response);
+    });
 });
 
