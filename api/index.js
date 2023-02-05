b6f4e225ca3c2389551d66a00be80bf7ca7dc7db
WanderingSnail <seongjin123.SY@gmail.com>
2023-02-04 22:14:35 -0800

    almost prototype

--- api/index.js
+++ api/index.js
@@ -1,8 +1,39 @@
+// const request = require('request');
+// const express = require('express');
+
+// let ingrList = 'chicken,tomato,pepper,cheese';
+// const app = express();
+
+// //routes 
+// app.get('/api', (req, res) => {
+
+//     const options = {
+//         method: 'GET',
+//         url: 'https://api.spoonacular.com/recipes/findByIngredients',
+//         qs: {
+//             apiKey: '3eb087ca38c948188eb44e538c7f21ff',
+//             ingredients: req.query.ingr,
+//             ignorePantry: 'true',
+//             ranking: '2'
+//         }
+//     };
+//     request(options, function (error, response, body) {
+//         if (error) throw new Error(error);
+//         console.log(req);
+//         console.log(body);
+
+//         res.json(JSON.parse(body));
+//     });
+// });
+
+// module.exports = app;
+
 const request = require('request');
 const express = require('express');
 
 let ingrList = 'chicken,tomato,pepper,cheese';
 const app = express();
+let keyCount = 0;
 
 //routes 
 app.get('/api', (req, res) => {
@@ -11,25 +42,62 @@ app.get('/api', (req, res) => {
         method: 'GET',
         url: 'https://api.spoonacular.com/recipes/findByIngredients',
         qs: {
-            apiKey: '0ce380dcbd274d5b8203bbca9a7cb1b8',
+            apiKey: '5b9e70cd3328410cb3e68f3a26212f08',
             ingredients: req.query.ingr,
             ignorePantry: 'true',
-            ranking: '2'
+            ranking: '1',
+            number: '10'
         }
     };
+    let idString = '';
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
-        console.log(req);
-        console.log(body);
 
-        res.json(JSON.parse(body));
+        // console.log(req);
+        // console.log("hello1");
+        body = JSON.parse(body);
+
+        console.log("hello2");
+        console.log(body[0]);
+        console.log(body[0]['id']);
+        keyCount = Object.keys(body).length;
+
+        for (let i = 0; i < keyCount; i++) {
+            if ((i < keyCount) && (i > 0)) {
+                idString += ',';
+            }
+            idString += body[i].id;
+        }
+        // idString = '' + body[0].id + ',' + body[1].id + ',' + body[2].id;
+        //+ ',' + body[3].id + ',' + body[4].id + ',' + body[5].id + ',' + body[6].id + ','
+        // + body[7].id + ',' + body[8].id + ',' + body[9].id;
+
+        // body = idString;
+        // res.json(JSON.parse(body));
+        // console.log(idString);
+        //res.json(JSON.parse(body));
+
+
+        const options2 = {
+            method: 'GET',
+            url: 'https://api.spoonacular.com/recipes/informationBulk',
+            qs: {
+                apiKey: '5b9e70cd3328410cb3e68f3a26212f08',
+                ids: idString,
+                includeNutrition: 'false'
+            }
+        };
+
+        request(options2, function (error, response, body) {
+            if (error) throw new Error(error);
+            // console.log("helloHEHE");
+            console.log(JSON.parse(body));
+            // console.log("bodyFinish");
+            res.json(JSON.parse(body));
+        });
     });
+
+
 });
 
 module.exports = app;
-
-// export 
-
-// app.listen(5050, () => {
-//     console.log('Running project on port $(port)');
-// });
--- public/script.js
+++ public/script.js
@@ -8,20 +8,46 @@ function callAPI(searchInput) {
 function searchRecipes() {
     let searchInput = document.getElementById('search').value;
 
-    //var searchList = searchInput.split(', ')
+    let node = document.getElementById('result');
+    while (node != undefined) {
+        document.getElementById('result').remove();
+        node = document.getElementById('result');
+    }
+
+    let x;
 
     fetch('./api?ingr=' + searchInput, options)
         .then(response => response.json())
         .then(response => {
-            document.body.innerHTML += response[0].title;
-            document.body.innerHTML += `<img src="${response[0].image}" />`;
+            console.log(response)
+            let i = 0;
+            document.body.innerHTML += '<id="result br>';
+            document.body.innerHTML += '<id="result br>';
+            document.body.innerHTML += '<id="result br>';
 
+            while (i < 5) {
+                document.body.innerHTML +=
+                    '<article id="result" class="anarticle">' +
+                    '<h2>' +
+                    '<a id="lnk" href="' + response[i].spoonacularSourceUrl + '" target="_blank">' + response[i].title + '</a>' +
+                    '</h2>' +
+                    '<br>' + '<br>' + '<br>' +
+                    // '<h2>' + response[i].title + '</h2>' +
+                    '<img src="' + response[i].image + '" />' +
+                    '<p>' + response[i].summary + '" </p>' +
+
+                    '<br>' + '<br>' + '<br>' + '<br>' + '<br>' + '<br>' + '<br>' +
+
+                    '</article>';
+
+                document.body.innerHTML += '<id="result" br>';
+                document.body.innerHTML += '<id="result" br>';
+                i += 1;
+            }
         })
         .catch(err => console.error(err))
+    window.scrollBy(0, 1000);
 }
-
-
-
 window.addEventListener("scroll", function () {
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
